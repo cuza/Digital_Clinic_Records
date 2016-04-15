7cba91ead29b1b6756f6d1a800f9e3763d8eae6d
Dave Cuza <dave.cuza90@gmail.com>
2016-04-15 13:46:08 -0400

    Project Bundles Added

---
+++ src/APIBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace APIBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('APIBundle:Default:index.html.twig');
+    }
+}
---
+++ src/BackendBundle/Entity/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BackendBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="`user`")
+ * @ORM\Entity(repositoryClass="BackendBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+}
