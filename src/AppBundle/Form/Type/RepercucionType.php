70ae6b8e20ce71081ac5fcf99a2f86ce2f362f05
Andy <andy.gonzalez2010@gmail.com>
2016-05-24 00:24:12 -0400

    vendor fixes

--- src/AppBundle/Form/Type/RepercucionType.php
+++ src/AppBundle/Form/Type/RepercucionType.php
@@ -5,16 +5,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
-class EstadoEgresoType extends AbstractType
+class RepercucionType extends AbstractType
 {
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'choices' => array(
-                'Sin Incapacidad' => 'Sin Incapacidad',
-                'Incapacidad temporal' => 'Incapacidad temporal',
-                'Incapacidad Definitiva' => 'Incapacidad Definitiva',
-
+                'Vivo' => 'Vivo',
+                'Fallecido' => 'Fallecido',
             )
         ));
 
