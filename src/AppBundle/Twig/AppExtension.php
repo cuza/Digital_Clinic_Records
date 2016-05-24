2b504fcf34d1185c2967d97902a89c5d6bd9b1df
Andy <andy.gonzalez2010@gmail.com>
2016-05-23 20:59:50 -0400

    vendor fixes

---
+++ src/AppBundle/Twig/AppExtension.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 5/23/2016
+ * Time: 8:54 PM
+ */
+
+namespace AppBundle\Twig;
+
+
+class AppExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('class', array($this, 'classFilter')),
+        );
+    }
+
+    public function classFilter($entity)
+    {
+        return get_class($entity);
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'app_extension';    }
+}
