d059b2614a20742f356714a73f9dc4683a642ba6
Andy <andy.gonzalez2010@gmail.com>
2016-05-25 00:23:43 -0400

    vendor fixes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -18,5 +19,17 @@ class DefaultController extends Controller
         return $this->render('AppBundle:Default:index.html.twig');
     }
 
-
+    /**
+     * @Security("has_role('ROLE_USER')")
+     * @Template()
+     * @Route("/search")
+     */
+    public function searchAction()
+    {
+        $em=$this->getDoctrine()->getManager();
+        $pacientes = $em->getRepository('AppBundle:Paciente')->findAll();
+        return array(
+            'pacientes'=>$pacientes
+        );
+    }
 }
