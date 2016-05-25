3327110584cd3469285d5279e5e2ea240ef03544
Andy <andy.gonzalez2010@gmail.com>
2016-05-25 00:48:58 -0400

    vendor fixes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class DefaultController extends Controller
@@ -23,13 +24,25 @@ class DefaultController extends Controller
      * @Security("has_role('ROLE_USER')")
      * @Template()
      * @Route("/buscar")
+     * @param Request $request
+     * @return array
+     * @throws \InvalidArgumentException
      */
-    public function searchAction()
+    public function searchAction(Request $request)
     {
-        $em=$this->getDoctrine()->getManager();
-        $pacientes = $em->getRepository('AppBundle:Paciente')->findAll();
+        $q=$request->query->get('q');
+        $em = $this->getDoctrine()->getManager();
+        $pacientes = $em->getRepository('AppBundle:Paciente')
+            ->createQueryBuilder('p')
+            ->orWhere("p.cId = :q")
+            ->orWhere("p.nombre = :q")
+            ->orWhere("p.primerApellido = :q")
+            ->orWhere("p.segundoApellido = :q")
+            ->setParameter('q', $q)
+        ->getQuery()->getResult();
+        dump($pacientes);
         return array(
-            'pacientes'=>$pacientes
+            'pacientes' => $pacientes
         );
     }
 }
